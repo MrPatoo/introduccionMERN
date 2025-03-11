6d3507063401ea06a328f5924ad5770560c378ee
Rodrigo <rjcm1209.06@gmail.com>
2025-03-11 11:26:25 -0600

    Crud clients v.1 (solo el modelo)

---
+++ backend/src/models/Clients.js
@@ -0,0 +1,73 @@
+/*fields:
+    name
+  lastName
+  birthday
+  email
+  password
+  telephone
+  dui
+  isVerified (esto es booleano)
+*/
+
+import { Schema, model } from "mongoose";
+
+const clientsSchema = new Schema({
+    name:{
+        type: String,
+        require: true
+    },
+
+    lastName:{
+        type: String,
+        require: true
+    },
+
+    birthday:{
+        type: Date,
+        require: true,
+        max:[new Date(), "No puede ser una fecha futura"]
+    },
+
+    email:{
+        type: String,
+        require: true,
+        unique: true,
+        match:[
+            /^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]{2,6}$/,
+            "Por favor ingrese un correo electronico valido", //validar el correo electronico
+        ],
+    },
+
+
+    password:{
+        type: String,
+        require: true,
+        minlenght: 6 //para poner un minimo de caracteres
+    },
+
+    telephone:{
+        type: String,
+        require: true,
+        unique: true,
+        match: [/^[0-9]{8}$/, "el numero de teléfono tiene que ser válido"] //validar número de teléfono
+    },
+
+    dui:{
+        type: String,
+        require: true,
+        unique: true
+        //match: [/^\d{8}-\d$/, "número de dui no válido"] //validar dui
+    },
+
+
+    isVerified:{
+        type: Boolean,
+        require: true
+    }
+},{
+    //tabla auditoria PRO
+    timestamps: true,
+    strict: false
+})
+
+export default model("Clients", clientsSchema);
