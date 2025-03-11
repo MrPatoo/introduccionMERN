39b230e43bb311d3f077efc1186cb93ba8a4eb01
Rodrigo <rjcm1209.06@gmail.com>
2025-03-11 14:40:02 -0600

    Crud clients v.2 (terminado y funcionando)

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@
 import express from "express";
 import ProductsRoutes from "./src/routes/products.js"
 import EmployeesRoutes from "./src/routes/employees.js"
+import ClientsRoutes from "./src/routes/clients.js"
 
 //Creo una constante que es igual a la libreria que acabo de crear, y la ejecuto.
 const app = express();
@@ -11,6 +12,7 @@ app.use(express.json());
 
 app.use("/api/products", ProductsRoutes); 
 app.use("/api/employee", EmployeesRoutes);
+app.use("/api/clients", ClientsRoutes);
 
 //Exporto la constante para usar express en todos lados.
 export default app;
---
+++ backend/src/controllers/clientsController.js
@@ -0,0 +1,37 @@
+const clientsController = {};
+
+import Clients from "../models/Clients.js";
+import clientsModel from "../models/Clients.js";
+
+//SELECT
+clientsController.getClients = async(req, res) =>{
+    const clients = await clientsModel.find();
+
+    res.json(clients)
+}
+
+//INSERT
+clientsController.postClients = async(req, res) =>{
+    const{name, lastName, birthday, email, password, telephone, dui, isVerified } = req.body;
+    const newClient = new clientsModel({name, lastName, birthday, email, password, telephone, dui, isVerified})
+    await newClient.save()
+
+    res.json({message: "Client saved"})
+}
+
+//DELETE
+clientsController.deleteClients = async(req,res) =>{
+    await clientsModel.findByIdAndDelete(req.param.id)
+
+    res.json({message: "Client deleted"})
+}
+
+//UPDATE
+clientsController.putClients = async(req, res)=>{
+    const{name, lastName, birthday, email, password, telephone, dui, isVerified } = req.body;
+    const updateClients = await clientsModel.findByIdAndUpdate(req.params.id, {name, lastName, birthday, email, password, telephone, dui, isVerified}, {new: true})
+
+    res.json({message: "Client updated successfully"})
+}
+
+export default clientsController;
