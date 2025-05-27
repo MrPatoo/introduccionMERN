e566ab2090718c90322257e2e113b16ce99b4d56
Rodrigo <rjcm1209.06@gmail.com>
2025-05-27 09:10:12 -0600

    otra vez el boton

--- frontend/src/components/Branches/CardBranches.jsx
+++ frontend/src/components/Branches/CardBranches.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Button from "../Button";
+import Button from "../Button1";
 
 const CardBranches = ({ branches, deleteBranch, updateBranch }) => {
   if (!branches) {
--- frontend/src/components/Products/CardProducts.jsx
+++ frontend/src/components/Products/CardProducts.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Button from "../Button";
+import Button from "../Button1";
 
 const CardProducts = ({ product, deleteProduct, updateProduct }) => {
   if (!product) {
